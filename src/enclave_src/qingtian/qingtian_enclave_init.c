68fed5991f388a196d69fde63f75e59e9ca10be0
houmingyong <houmingyong@huawei.com>
2023-09-04 17:38:32 +0800

    prtect to access qtsm with mutexes

--- src/enclave_src/qingtian/qingtian_enclave_init.c
+++ src/enclave_src/qingtian/qingtian_enclave_init.c
@@ -17,6 +17,7 @@
 #include <sys/types.h>
 #include <sys/vfs.h>
 #include <linux/vm_sockets.h>
+#include <pthread.h>
 #include "secgear_log.h"
 #include <qtsm_lib.h>
 #include "qt_rpc_proxy.h"
@@ -25,34 +26,45 @@ extern __attribute__((weak)) int qtsm_lib_init(void);
 extern __attribute__((weak)) void qtsm_lib_exit(int qtsm_dev_fd);
 
 static int g_qtsm_fd;
+static pthread_mutex_t qtsm_fd_lock;
 extern cc_enclave_result_t handle_ecall_function(uint8_t *input, size_t input_len, uint8_t **output, size_t *output_len);
 
 static __attribute__((constructor)) void qt_enclave_init(void)
 {
     int ret = qt_rpc_proxy_init(VMADDR_CID_ANY, handle_ecall_function);
     if (ret != 0) {
-        PrintInfo(PRINT_DEBUG, "enclave proxy init failed\n");
-    }
-    PrintInfo(PRINT_DEBUG, "enclave proxy init success\n");
-    if (qtsm_lib_init == NULL) {
+        PrintInfo(PRINT_ERROR, "enclave proxy init failed\n");
         return;
     }
-    g_qtsm_fd = qtsm_lib_init();
-    if (g_qtsm_fd < 0) {
-        PrintInfo(PRINT_DEBUG, "enclave init qtsm lib failed\n");
-    }
+    PrintInfo(PRINT_DEBUG, "enclave proxy init success\n");
 }
 static __attribute__((destructor)) void qt_enclave_destroy(void)
 {
-    if (g_qtsm_fd > 0 && qtsm_lib_exit != NULL) {
-        qtsm_lib_exit(g_qtsm_fd);
-        g_qtsm_fd = 0;
-    }
     qt_rpc_proxy_destroy();
     PrintInfo(PRINT_DEBUG, "destroy enclave proxy\n");
 }
 
 int qt_get_qtsm_fd(void)
 {
+    if (qtsm_lib_init == NULL || qtsm_lib_exit == NULL) {
+        PrintInfo(PRINT_ERROR, "there is no symbol qtsm_lib_init or qtsm_lib_exit\n");
+        return -1;
+    }
+    pthread_mutex_lock(&qtsm_fd_lock);
+    g_qtsm_fd = qtsm_lib_init();
+    if (g_qtsm_fd < 0) {
+        pthread_mutex_unlock(&qtsm_fd_lock);
+    }
     return g_qtsm_fd;
+}
+
+void qt_release_qtsm_fd(int fd)
+{
+    if (fd == g_qtsm_fd) {
+        qtsm_lib_exit(g_qtsm_fd);
+        g_qtsm_fd = -1;
+        pthread_mutex_unlock(&qtsm_fd_lock);
+    }
+
+    return;
 }
