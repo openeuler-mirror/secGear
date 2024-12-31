66f43e19f2a7082a252f6b8f37bfd13a3ec552b8
chenjiayi <chenjiayi22@huawei.com>
2024-12-31 17:43:35 +0800

    fix incorrect tee name in policy engine and add unit test case

--- service/attestation/attestation-service/resource/src/policy/opa/mod.rs
+++ service/attestation/attestation-service/resource/src/policy/opa/mod.rs
@@ -16,7 +16,7 @@ use crate::{
 };
 use anyhow::Context;
 use async_trait::async_trait;
-use std::path::PathBuf;
+use std::{fs::read_to_string, path::PathBuf};
 
 const DEFAULT_RESOURCE_POLICY_DIR: &str = "/run/attestation/attestation-service/resource/policy/";
 const DEFAULT_RESOURCE_VIRTCCA_DEFAULT_POLICY: &str = "default_virtcca.rego";
@@ -45,7 +45,7 @@ impl PolicyEngine for OpenPolicyAgent {
         if let Some(tee) = claim_json.get("tee") {
             if let Some(tee_str) = tee.as_str() {
                 match tee_str {
-                    "virtcca" => {
+                    "vcca" => {
                         engine
                             .add_policy_from_file(
                                 self.base.join(DEFAULT_RESOURCE_VIRTCCA_DEFAULT_POLICY),
@@ -73,5 +73,49 @@ impl PolicyEngine for OpenPolicyAgent {
     /// Read the policy content from the file.
     async fn get_policy(&self, _path: &str, _policy: &str) -> Result<String> {
         Err(ResourceError::NotImplemented)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use tokio::runtime;
+
+    use super::{OpenPolicyAgent, PolicyEngine};
+
+    #[test]
+    fn test_evaluate() {
+        let pwd = std::env::current_dir().expect("failed to get pwd");
+        let opa = OpenPolicyAgent::new(pwd.join("src/policy/opa"));
+        let resource = "b/p/f";
+        let claims = r#"
+{
+    "iss": "oeas",
+    "iat": 1735635443,
+    "nbf": 1735635443,
+    "exp": 1735635743,
+    "evaluation_reports": {
+        "eval_result": true,
+        "policy": [],
+        "report": {
+            "default_vcca.rego": "{\"vcca.cvm.rim\":\"1ee366339c8245a34a8ad9d27a0b912a588af7da8aef514ae8dec22746956dd1\"}",
+            "ima": {}
+        }
+    },
+    "tee": "vcca",
+    "tcb_status": {
+        "vcca.cvm.challenge": "586667776b4972524b58684550524f384771654c7244695356485134715f372d4e36375064587a50457763000000000000000000000000000000000000000000",
+        "vcca.cvm.rem.0": "927b62bc7f4d9fd03afd0b9b2fe8832004b570b4c4bffc2949c4e461b0a0ff63",
+        "vcca.cvm.rem.1": "0000000000000000000000000000000000000000000000000000000000000000",
+        "vcca.cvm.rem.2": "0000000000000000000000000000000000000000000000000000000000000000",
+        "vcca.cvm.rem.3": "0000000000000000000000000000000000000000000000000000000000000000",
+        "vcca.cvm.rim": "1ee366339c8245a34a8ad9d27a0b912a588af7da8aef514ae8dec22746956dd1",
+        "vcca.cvm.rpv": "01000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+        "vcca.platform": ""
+    }   
+}"#;
+        let policy = vec![];
+        let rt = runtime::Runtime::new().unwrap();
+        let r = rt.block_on(opa.evaluate(resource, claims, policy));
+        assert_eq!(r.unwrap(), true);
     }
 }
