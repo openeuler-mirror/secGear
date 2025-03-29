ce0a4ddbae322eda4deed8e1376f79f543e51a97
SPYFAMILY <xiexiunian@h-partners.com>
2025-03-29 14:39:48 +0800

    更新docker基础镜像为24.03，使用secger rpm包构建完整as

--- service/attestation/oeas/service/build.sh
+++ service/attestation/oeas/service/build.sh
@@ -1,43 +1,40 @@
 #!/usr/bin/bash
 set -e
 
-VERSION="22.03-lts-sp4"
+# secgear rpm包仓库
+SECGEAR_REPO_URL="http://121.36.84.172/dailybuild/EBS-openEuler-25.03/openeuler-2025-03-29-09-24-45/everything/aarch64/"
+
+VERSION="24.03-lts"
 VERSION_UPPER=$(echo "$VERSION" | tr '[:lower:]' '[:upper:]')
 RELEASE="openEuler-${VERSION_UPPER}"
-CONTAINER_NAME="oeas"
 
 ARCH=$(uname -m)
-DATA_DIR=$(readlink -f "$(dirname "$(dirname "$PWD")")")
-
-DOCKER_TAR="openEuler-docker.${ARCH}.tar.xz"
 OPENEULER_DOCKER_URL="https://repo.openeuler.org/${RELEASE}/docker_img/${ARCH}"
+DOCKER_TAR="openEuler-docker.${ARCH}.tar.xz"
 IMAGE_NAME="openeuler-${VERSION}"
+CONTAINER_NAME="oeas"
 
 echo "正在获取镜像 openeuler 镜像"
-if [ -z "$(docker images -q "$IMAGE_NAME")" ]; then
+if [ -z "$(docker images -q "openeuler/openeuler:${VERSION}")" ]; then
   echo "下载镜像文件: ${OPENEULER_DOCKER_URL}/${DOCKER_TAR}"
-  wget "${OPENEULER_DOCKER_URL}/${DOCKER_TAR}"
+  curl -O "${OPENEULER_DOCKER_URL}/${DOCKER_TAR}"
   docker load -i "${DOCKER_TAR}"
 fi
 docker tag "$IMAGE_NAME" "openeuler/openeuler:${VERSION}"
-echo "openeuler 镜像加载完成，正在拉取 rust 镜像"
+echo "openeuler 镜像加载完成，正在构建镜像"
 
-if [ -z "$(docker images -q "rust")" ]; then
-  docker pull rust
-fi
-echo "rust 镜像拉取成功，开始构建 ${CONTAINER_NAME} 镜像"
-
-DOCKER_BUILDKIT=1 docker build --build-context data="$DATA_DIR" -t "$CONTAINER_NAME" .
+DOCKER_BUILDKIT=1 docker build --build-arg VERSION=${VERSION} \
+    --build-arg SECGEAR_REPO_URL=${SECGEAR_REPO_URL} -t "$CONTAINER_NAME" .
 echo "${CONTAINER_NAME} 镜像构建完成，正在启动容器"
 
-mkdir -p /etc/attestation /var/log/attestation
-chmod -R 777 /etc/attestation /var/log/attestation
-docker run -itd --name "$CONTAINER_NAME" -p 80:5000 -p 127.0.0.1:8080:8080  -v /etc/attestation:/etc/attestation -v /var/log/attestation:/opt/oeas/logs "$CONTAINER_NAME"
+docker volume create attestation_volume
+docker run -itd --name "$CONTAINER_NAME" -p 80:5000 -p 8080:8080  \
+    -v attestation_volume:/etc/attestation:rw "$CONTAINER_NAME"
 echo "容器启动成功"
 
 echo "等待服务启动..."
 for i in {1..10}; do
-  if curl -s -o /dev/null -w "%{http_code}" "http://127.0.0.1:5000/challenge" | grep -q "200"; then
+  if curl -s -o /dev/null -w "%{http_code}" "http://127.0.0.1/oeas-api/challenge" | grep -q "200"; then
     echo "服务启动成功"
     break
   else
